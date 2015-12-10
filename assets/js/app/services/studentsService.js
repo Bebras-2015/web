b9dc6675793e74037f30c8a373ece6d36abf5237
Vytenis <vytenis.kuciauskas@nfq.lt>
2015-12-10 11:05:16 +0200

    Mess cleanup

--- assets/js/app/services/studentsService.js
+++ assets/js/app/services/studentsService.js
@@ -32,7 +32,42 @@ myApp.service('studentsService', [function () {
             return score;
         },
         set score(val) {
-            score = '★ ' + val;
+            switch (val) {
+                case 1:
+                    score = '❶';
+                    break;
+                case 2:
+                    score = '❷';
+                    break;
+                case 3:
+                    score = '❸';
+                    break;
+                case 4:
+                    score = '❹';
+                    break;
+                case 5:
+                    score = '❺';
+                    break;
+                case 6:
+                    score = '❻';
+                    break;
+                case 7:
+                    score = '❼';
+                    break;
+                case 8:
+                    score = '❽';
+                    break;
+                case 9:
+                    score = '❾';
+                    break;
+                case 10:
+                    score = '❿';
+                    break;
+                default:
+                    score = '★ ' + val;
+                    break;
+            }
+
         }
 
     };
