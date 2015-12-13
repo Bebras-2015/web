294cd0f55ab25a85b8725ebdd477e0594d1ace3f
Vytenis <vytenis.kuciauskas@nfq.lt>
2015-12-14 00:12:18 +0200

    Added the loading indicator more info here https://github.com/chieffancypants/angular-loading-bar

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -1,8 +1,16 @@
-var myApp = angular.module('myApp', ['ngResource', 'ngLodash', 'ngRoute', 'chart.js']);
+var myApp = angular.module('myApp', ['ngResource', 'ngLodash', 'ngRoute', 'chart.js', 'angular-loading-bar']);
 
 myApp.config([
-    '$routeProvider', '$locationProvider',
-    function ($routeProvider, $locationProvider) {
+    '$routeProvider', '$locationProvider', 'cfpLoadingBarProvider',
+    function ($routeProvider, $locationProvider, cfpLoadingBarProvider) {
+
+        cfpLoadingBarProvider.includeSpinner = false;
+        cfpLoadingBarProvider.parentSelector = 'loading';
+        cfpLoadingBarProvider.loadingBarTemplate = '<div id="loading-bar" class="progress">' +
+            '<div class="progress-bar" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100">' +
+            '</div>' +
+            '</div>';
+
         $locationProvider.html5Mode({
             enabled: true,
             requireBase: false
